a698c274525c1cbf409412720b27678c30cc9a12
raunak <raunakbiswas05@gmail.com>
2025-04-12 17:28:23 +0530

    slider for dashboard(semi complete)

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -9,19 +9,36 @@ export default async function Dashboardroute() {
             <div className="text-4xl">
                 this is dashboard
             </div>
-            <div>
-                <DashboardSelect data={data}>
-
-                </DashboardSelect>
+            <div className="mt-10">
+                <DashboardSelect data={data} />
             </div>
         </>
     );
 }
 
 // some dummy deta
-const data = {
-    id: "webdev",
-    name: "web dev",
-    description: "Web development is the process of creating websites and web applications that run on the internet. It involves both front-end development, which focuses on the design and user experience using technologies like HTML, CSS, and JavaScript, and back-end development, which handles the server-side logic and databases using languages such as Python, PHP, Ruby, or Node.js. Web developers work to ensure websites are functional, responsive, and user-friendly across different devices and browsers. With the growing demand for online services, web development has become a crucial skill in the tech industry, combining creativity and technical expertise to build everything from simple landing pages to complex web-based platforms",
-    image: "/images/image.png",
-}
+const data = [{
+        id: "webdev",
+        name: "web dev",
+        description: "Web development is the process of creating websites and web applications that run on the internet. It involves both front-end development, which focuses on the design and user experience using technologies like HTML, CSS, and JavaScript, and back-end development, which handles the server-side logic and databases using languages such as Python, PHP, Ruby, or Node.js. Web developers work to ensure websites are functional, responsive, and user-friendly across different devices and browsers. With the growing demand for online services, web development has become a crucial skill in the tech industry, combining creativity and technical expertise to build everything from simple landing pages to complex web-based platforms",
+        image: "/images/image.png",
+    },
+    {
+        id: "webdev",
+        name: "web dev",
+        description: "Web development is the process of creating websites and web applications that run on the internet. It involves both front-end development, which focuses on the design and user experience using technologies like HTML, CSS, and JavaScript, and back-end development, which handles the server-side logic and databases using languages such as Python, PHP, Ruby, or Node.js. Web developers work to ensure websites are functional, responsive, and user-friendly across different devices and browsers. With the growing demand for online services, web development has become a crucial skill in the tech industry, combining creativity and technical expertise to build everything from simple landing pages to complex web-based platforms",
+        image: "/images/image.png",
+    },
+    {
+        id: "webdev",
+        name: "web dev",
+        description: "Web development is the process of creating websites and web applications that run on the internet. It involves both front-end development, which focuses on the design and user experience using technologies like HTML, CSS, and JavaScript, and back-end development, which handles the server-side logic and databases using languages such as Python, PHP, Ruby, or Node.js. Web developers work to ensure websites are functional, responsive, and user-friendly across different devices and browsers. With the growing demand for online services, web development has become a crucial skill in the tech industry, combining creativity and technical expertise to build everything from simple landing pages to complex web-based platforms",
+        image: "/images/image.png",
+    },
+    {
+        id: "webdev",
+        name: "web dev",
+        description: "Web development is the process of creating websites and web applications that run on the internet. It involves both front-end development, which focuses on the design and user experience using technologies like HTML, CSS, and JavaScript, and back-end development, which handles the server-side logic and databases using languages such as Python, PHP, Ruby, or Node.js. Web developers work to ensure websites are functional, responsive, and user-friendly across different devices and browsers. With the growing demand for online services, web development has become a crucial skill in the tech industry, combining creativity and technical expertise to build everything from simple landing pages to complex web-based platforms",
+        image: "/images/image.png",
+    }
+]
